dbfa19313cc9d4b8da2950dd4c505f8a4fe083f0
Carlostala04 <carlostala.dev@gmail.com>
2026-03-18 15:49:01 -0600

    se uso el index para una prueba

--- my-farmer-frontend/src/app/index.tsx
+++ my-farmer-frontend/src/app/index.tsx
@@ -1,18 +1,12 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-import Skeleton from '@/components/skeleton';
+import NavBar from '@/components/navBar';
 
 export default function HomeScreen() {
   return (
-   <Skeleton/>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavBar />
+    </SafeAreaView>
   );
 }
 

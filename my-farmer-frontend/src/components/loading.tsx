370e6a7443471566d9f53ad19e0d3ca70bef124b
Carlostala04 <carlostala.dev@gmail.com>
2026-03-18 21:54:50 -0600

    se creo el loading

--- my-farmer-frontend/src/components/loading.tsx
+++ my-farmer-frontend/src/components/loading.tsx
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Colors from "@/constants/colors";
+import React, { useEffect, useRef } from "react";
+import { View, Animated, Easing, StyleSheet } from "react-native";
 
-const loading = () => {
+export default function CustomSpinner() {
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const animation = Animated.loop(
+      Animated.timing(rotateAnim, {
+        toValue: 1,
+        duration: 900,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    );
+
+    animation.start();
+
+    return () => animation.stop();
+  }, [rotateAnim]);
+
+  const spin = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.spinner,
+          {
+            transform: [{ rotate: spin }],
+          },
+        ]}
+      />
     </View>
-  )
+  );
 }
 
-export default loading
+const styles = StyleSheet.create({
+  container: {
+    width:"100%",
+    height:"100%",
+    display:"flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  spinner: {
+    width: 50,
+    height: 50,
+    borderWidth: 5,
+    borderColor: Colors.PRIMARY_GREEN,
+    borderTopColor: Colors.INPUT_BORDER,
+    borderRadius: 999,
+  },
+});

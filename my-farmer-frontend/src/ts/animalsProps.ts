e48b7916ef5787d56299ca7d91e922ae0d04b5d9
Carlostala04 <carlostala.dev@gmail.com>
2026-03-20 23:22:59 -0600

    se creo la interfaz para tener las propiedades de los animales

---
+++ my-farmer-frontend/src/ts/animalsProps.ts
@@ -0,0 +1,11 @@
+export interface animalsProps{
+    id:number,
+    nombre:string,
+    raza:string,
+    sexo:string,
+    color:string,
+    peso:number,
+    estado:string,
+    imagen:string,
+    edad:number
+}

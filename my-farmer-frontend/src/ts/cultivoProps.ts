932c32de5009886b6676c0c5b181e77f8ec465f0
Carlostala04 <carlostala.dev@gmail.com>
2026-03-22 11:12:05 -0600

    se cambiaron las props de los cultivos temporalmente

--- my-farmer-frontend/src/ts/cultivoProps.ts
+++ my-farmer-frontend/src/ts/cultivoProps.ts
@@ -1,9 +1,10 @@
 export interface cultivoProps{
     id:number,
     nombre:string,
-    fechaSiembra: Date,
-    fechaCosecha:Date,
+    fechaSiembra: string,
+    fechaCosecha:string,
     estado:boolean,
     foto:string,
-    
+    imagen:string,
+    ubicacion:string
 }

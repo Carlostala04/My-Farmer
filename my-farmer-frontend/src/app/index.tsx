5c6fd9d57b5365b60f41799c69a32e6e34e06948
Carlostala04 <carlostala.dev@gmail.com>
2026-03-24 21:38:45 -0600

    se cambio el index para que la primera pantalla que aparezca sea la de los animales

--- my-farmer-frontend/src/app/index.tsx
+++ my-farmer-frontend/src/app/index.tsx
@@ -1,12 +1,13 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import NavBar from '@/components/navBar';
+import AnimalsScreen from './(tabs)/animals';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <NavBar />
-    </SafeAreaView>
+    
+      <AnimalsScreen/>
+    
   );
 }
 

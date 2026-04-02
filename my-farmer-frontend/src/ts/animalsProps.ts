0f3a3da4334a6fe7bfdeecc22db320d853974200
Carlostala04 <carlostala.dev@gmail.com>
2026-04-01 21:39:39 -0600

    se cambiaron las props de los animales

--- my-farmer-frontend/src/ts/animalsProps.ts
+++ my-farmer-frontend/src/ts/animalsProps.ts
@@ -1,11 +1,15 @@
-export interface animalsProps{
-    id:number,
-    nombre:string,
-    raza:string,
-    sexo:string,
-    color:string,
-    peso:number,
-    estado:string,
-    imagen:string,
-    edad:number
+export interface animalsProps {
+  id: number;
+  nombre: string;
+  raza?: string;
+  categoria?: string;
+  color?: string;
+  fecha_nacimiento?: Date;
+  peso?: number;
+  peso_unidad?: string;
+  altura?: number;
+  estado?: string;
+  notas?: string;
+  foto?: string;
+  parcela_id?: string;
 }

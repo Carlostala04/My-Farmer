2f15bf14f5188ae7f253708b199a701698d45e18
Carlostala04 <carlostala.dev@gmail.com>
2026-04-03 23:26:01 -0600

    se creo el archvio para crear la pantalla de suscripcion

---
+++ my-farmer-frontend/src/app/(tabs)/suscripcion.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import NavBar from "@/components/navBar";
+export default function Suscripcion() {
+  return (
+    <View>
+        
+      <NavBar />
+    </View>
+  )
+}

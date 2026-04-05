83b30f8bdcc9115218fe16c937ed3f5d35e6351c
Carlostala04 <carlostala.dev@gmail.com>
2026-04-04 19:54:03 -0600

    se creo el icono de editar

---
+++ my-farmer-frontend/src/components/ui/editIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import Svg, { Path, SvgProps } from "react-native-svg"
+
+const EditIcon = (props: SvgProps) => (
+  <Svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <Path
+      fill="currentColor"
+      d="M20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.84 1.83 3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25z"
+    />
+  </Svg>
+)
+
+export default EditIcon

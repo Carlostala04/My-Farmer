b1558c51d5874718e3503ad967ace007d1ba84ca
Carlostala04 <carlostala.dev@gmail.com>
2026-04-06 11:43:04 -0600

    se creo la pantalla para el historial del animal

---
+++ my-farmer-frontend/src/app/(tabs)/historialAnimales.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function HistorialAnimales() {
+  return (
+    <View>
+      <Text>historialAnimales</Text>
+    </View>
+  )
+}
